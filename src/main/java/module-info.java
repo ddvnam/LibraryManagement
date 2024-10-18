82eac1f6a13dc6d8411d320b0fb3fc1340dddf9c
ddvnam <23020402@vnu.edu.vn>
2024-10-18 07:41:32 +0700

    update module.-infor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.librarymanagement2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires java.mail;
 
     opens com.example.librarymanagement2 to javafx.fxml;
     exports com.example.librarymanagement2;
